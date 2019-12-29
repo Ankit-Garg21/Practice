1cbd4b1a58c5cb1d4aa4c0e387bba5d5f22068a6
debansal <er.deepikabansal@gmail.com>
2019-12-30 00:27:35 +0530

    Restructure

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 // const linearSearch = require('./search/linear_search');
-import linearSearch from './search/linear_search';
-import binarySearch from './search/binary_search';
-import quickSort from './sort/quick_sort';
-import Stack from './stack/stack';
-import RainWaterTrap from './practice/rain_water_trap';
-import LNRS from './practice/lnrs';
-import FNRC from './practice/fnrc';
-import FLN from './practice/fln';
-import MaxWaterBottles from './practice/max_water_bottles';
-import MinNumberOfMoves from './practice/min_number_of_moves';
-import BiggestNumber from './practice/biggest_number';
-import MaxConsecutiveRepeatingChar from './practice/max_consecutive_repeating_char';
-import RunLengthEncoding from './practice/run_length_encoding';
-import CountWaysForStairs from './practice/count_ways_stairs';
-
-const items = [1, 2, 3, 12];
-let indices;
+import linearSearch from './dsa/search/linear_search';
+import binarySearch from './dsa/search/binary_search';
+import quickSort from './dsa/sort/quick_sort';
+import Stack from './dsa/stack/stack';
+import RainWaterTrap from './dsa/practice/rain_water_trap';
+import LNRS from './dsa/practice/lnrs';
+import FNRC from './dsa/practice/fnrc';
+import FLN from './dsa/practice/fln';
+import MaxWaterBottles from './dsa/practice/max_water_bottles';
+import MinNumberOfMoves from './dsa/practice/min_number_of_moves';
+import BiggestNumber from './dsa/practice/biggest_number';
+import MaxConsecutiveRepeatingChar from './dsa/practice/max_consecutive_repeating_char';
+import RunLengthEncoding from './dsa/practice/run_length_encoding';
+import CountWaysForStairs from './dsa/practice/count_ways_stairs';
 
 // console.log("***************Linear Search*************");
-// indices = linearSearch(items, 12);
+// const items = [1, 2, 3, 12];
+// const indices = linearSearch(items, 12);
 // console.log(indices);
 
 // console.log("***************Binary Search*************");
-// indices = binarySearch([1, 5, 10, 12, 14, 17, 22, 100], 22);
+// const indices = binarySearch([1, 5, 10, 12, 14, 17, 22, 100], 22);
 // console.log(indices);
 
 // console.log("*****************Quick Sort****************");
-// indices = quickSort([93,31,27,65,90,12,14,56], 0, 7);
-// console.log(indices);
+// const res = quickSort([93,31,27,65,90,12,14,56], 0, 7);
+// console.log(res);
 
 // console.log("******************Stack*****************");
 // let obj = new Stack();
@@ -39,7 +37,7 @@ let indices;
 // console.log("*****************Rain Water Trap*****************");
 // const arr = [3,0,0,2,0,4];
 // const quantity = RainWaterTrap(arr);
-// console.log(quantitiy);
+// console.log(quantity);
 
 // console.log("*****************Longest substring without repeating characters*****************");
 // const str = "GEEKSFORGEEKS";
@@ -80,7 +78,7 @@ let indices;
 // const res = RunLengthEncoding(str);
 // console.log(res);
 
-console.log("************Count ways to reach nth stair**************");
-let noOfStairs = 4, maxSteps = 3;
-const res = CountWaysForStairs(noOfStairs, maxSteps);
-console.log(res);
+// console.log("************Count ways to reach nth stair**************");
+// let noOfStairs = 4, maxSteps = 3;
+// const res = CountWaysForStairs(noOfStairs, maxSteps);
+// console.log(res);
